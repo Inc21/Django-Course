45543efebaf23edf0e988a2edf2ea9090d7831ed
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-06-28 01:03:06 +0100

    Complete lesson 25, Search

--- projects/views.py
+++ projects/views.py
@@ -5,10 +5,13 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Project
 from .forms import ProjectForm
+from .utils import search_projects
 
 
 def projects(request):
-    projects = Project.objects.all()
+
+    projects, search_query = search_projects(request)
+
     context = {'projects': projects}
     return render(request, 'projects/projects.html', context)
 
--- users/views.py
+++ users/views.py
@@ -3,8 +3,10 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.db.models import Q
 from .models import Profile
 from .forms import CustomUserCreationForm, ProfileForm, SkillForm
+from .utils import search_profiles
 
 
 def login_user(request):
@@ -76,8 +78,8 @@ def profiles(request):
     """
     view
     """
-    profiles = Profile.objects.all()
-    context = {'profiles': profiles}
+    profiles, search_query = search_profiles(request)
+    context = {'profiles': profiles, 'search_query': search_query}
     return render(request, 'users/profiles.html', context)
 
 
